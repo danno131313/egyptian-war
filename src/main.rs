6ce0ef0c7600d6984f3ea9997084871ca748b38c
Daniel Kavanagh <dan.kava13@gmail.com>
2017-11-05 11:41:13 -0800

    add ncurses dependency, add minimal main screen and character input recognition

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,49 @@
 extern crate cards;
+extern crate ncurses;
 mod player;
 use cards::deck::Deck;
-use player::Player;
+use ncurses::*;
+use std::process::exit;
 
 fn main() {
-    println!("Welcome to Egyptian War!");
+    initscr();
+    noecho();
+    raw();
+    let mut max_y = 0;
+    let mut max_x = 0;
+    getmaxyx(stdscr(), &mut max_y, &mut max_x);
+
+    mvprintw(3, (max_x / 2 - 11), "Welcome to Egyptian War!\n");
+
+    mvprintw(max_y / 2 - 1, 1, "Press spacebar to play!");
+    
+    let mut ch: i32 = 0;
+    while ch != 27 {
+        if ch == 32 {
+            play();
+        } else {
+            ch = getch();
+        }
+    }
+    endwin();
+    exit(0);
+}
+
+fn play() {
+    clear();
+    let mut max_y = 0;
+    let mut max_x = 0;
+    getmaxyx(stdscr(), &mut max_y, &mut max_x);
+
+    let mut player1 = Deck::new_empty();
+    let mut player2 = Deck::new_empty();
+
     let mut deck = Deck::new();
     deck.shuffle();
-    let mut player1 = Player::new();
-    println!("{}", deck.draw());
+
+    mvprintw(1, 1, format!("Player 1: {} cards left", player1.len()).as_ref());
+    mvprintw(1, max_x - 23, format!("Player 2: {} cards left", player2.len()).as_ref());
+    getch();
+    endwin();
+    exit(0);
 }
