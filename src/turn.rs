c417557a22c61f08a7cceb5c50ccc5edcb28b113
Danny Kavanagh <dankava13@gmail.com>
2018-02-10 21:39:16 -0800

    clean up spacing

--- src/turn.rs
+++ src/turn.rs
@@ -10,6 +10,7 @@ pub fn turn_handler(mut game: Game, turns: &mut usize, key: i32) -> Game {
         let next_p1_turn: bool;
         let curr_name: &str;
         let other_name: &str;
+
         if game.p1_turn == true {
             this_player = &mut game.player1;
             other_player = &mut game.player2;
@@ -25,21 +26,29 @@ pub fn turn_handler(mut game: Game, turns: &mut usize, key: i32) -> Game {
             curr_name = "Player 2";
             other_name = "player 1";
         }
+
         if game.face_off {
             if *turns == 0 {
                 clear();
+
                 mvprintw(game.max_y / 2, game.max_x / 2 - 21, format!("{} lost the face off! It's now {}'s turn.", curr_name, other_name).as_ref());
+
                 other_player.add_deck(&mut game.pile);
+
                 game.p1_turn = next_p1_turn;
                 game.face_off = false;
+
                 let mut cont = getch();
+
                 while cont != 32 {
                     cont = getch();
                 }
             } else {
                 if key == my_key {
                     let curr_card = this_player.draw().expect("Deck is empty!");
+
                     game.pile.add(curr_card);
+
                     if curr_card.is_face() {
                         game.p1_turn = next_p1_turn;
                         *turns = get_turns(&curr_card);
@@ -50,8 +59,11 @@ pub fn turn_handler(mut game: Game, turns: &mut usize, key: i32) -> Game {
             }
         } else if key == my_key {
             let curr_card = this_player.draw().expect("Deck is empty!");
+
             game.pile.add(curr_card);
+
             game.p1_turn = next_p1_turn;
+
             if curr_card.is_face() {
                 game.face_off = true;
                 *turns = get_turns(&curr_card);
